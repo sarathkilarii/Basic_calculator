7d425c035ab1ec13c05f473becca32899979350a
sarathkilari <121596593+sarathkilarii@users.noreply.github.com>
2022-12-29 06:32:57 +0100

    Add files via upload

---
+++ building_calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter first number: "))
+op = input("Enter operator:")
+num2 = float(input("enter second operator:"))
+if op == "+":
+    print("num1 + num2")
+elif op == "-":
+    print("num1 - num2")
+elif op == "/":
+    print("num1 / num2")
+elif op == "*":
+    print("num1 * num2")
+else:
+    print("error")
